fe3dcb5e4ac1f9f7e7d877b08938bdc6e138738d
Clément Llobet Jasparo <clementllobetjasparo@MBP-de-Clement.blueparis.fr>
2021-09-09 09:28:03 +0200

    projet terminé

--- main.js
+++ main.js
@@ -1,24 +1,58 @@
-/* 
-    MÉTHODE 1
-*/
+document.onreadystatechange = () => {
 
-const video = document.getElementById('video');
+    if(document.readyState === 'interactive') {
+        const
+            snapButton = document.getElementById('snap-button');
+            width = 640;
+            height = 480;
 
-const startup = () => {
 
+
+        const startup = () => {
+
+            navigator.mediaDevices.getUserMedia({
+                audio: false,
+                video: {
+                    width, height
+                }
+            })
+            .then(stream => {
+                video.srcObject = stream;
+                video.onloadedmetadata = () => {
+                    video.play();
+                }
+            })
+            .catch(console.error);
+        };
+
+        window.addEventListener('load', startup);
+
+
+
+    const takeAPicture = () => {
+        const canvas = document.querySelector('canvas');
+        const video = document.getElementById('video');
+        const photo = document.getElementById('photo');
+
+        const context = canvas.getContext('2d');
+
+        canvas.height = height;
+        canvas.width = width;
+
+        context.drawImage(video, 0, 0, width, height);
+
+        const data = canvas.toDataURL('image/png');
+        // const sliderImages = [];
+
+
+        photo.setAttribute('src', data);
+        photo.setAttribute('alt', data);
+    };
+
+    snapButton.addEventListener('click', () => {
+        takeAPicture();
+    });
+
+    }
     
-
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true
-    })
-    .then(stream => {
-        video.srcObject = stream;
-        video.onloadedmetadata = (ev) => {
-            video.play();
-        }
-    })
-    .catch(console.error);
 }
-
-window.addEventListener('load', startup, false);
